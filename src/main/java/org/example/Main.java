d88d3fe6955914b35832ab6e40627330ef20f3c2
Tushar EY <Tusha3@gds.ey.com>
2025-04-14 11:04:22 +0530

    Add keys in map

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -31,6 +31,9 @@ public class Main {
         Map<String, Integer> map = new HashMap<>();
         map.put("one", 1);
         map.put("two", 2);
+        map.put("three", 3);
+        map.put("four", 4);
+        map.put("five", 5);
 
         Map<String, Integer> map1 = new HashMap<>();
         map1.put("one", 1);
